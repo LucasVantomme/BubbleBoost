4ed210725ee3cf05668bd7e4881b19bf2d1650a3
Abdellatif Soussi <abdeelsoo@gmail.com>
2020-01-24 17:52:02 +0100

    Page Profil

---
+++ profil.php
@@ -0,0 +1,100 @@
+<?php 
+$page = 'Profil';
+$titre = 'BubbleBoost - Page Profil';
+include 'assets/include/header.php'; 
+
+?>
+
+<div class="row profil">
+    <div class="col-4">
+        <img src="assets/images/avatar/johnsnow.jpg" alt="..." class="img-thumbnail">
+        <button type="button" class="btn btn-info">Changer de photo</button>
+    </div>
+    <div class="col-8">
+        <h1>Nom et Prénom</h1>
+        <p>Membre depuis le 08 novembre 2018 </p>
+        <div class="profil_infos">
+            <p>Suit 6 bulles actuellemnt </p>
+            <p>A publié 0 bulles</p>
+            <button type="button" class="btn btn-info">Modifier mes informations</button>
+        </div> 
+    </div>
+    
+</div>
+
+<h2>Bulles Suivies</h2><br>
+<div class="row bulles_suivies">
+        <div class="col-6 ">
+            <img src="assets/images/avatar/default.jpg" alt="..." class="img-thumbnail">
+            <div class="bulles_content">
+                <h5>Aventure Particulière</h5>
+                <p>publié le 10/10/20</p>
+                <p><strong>Chapitre 12</strong></p>
+            </div>
+        </div>
+         <div class="col-6">
+            <img src="assets/images/avatar/default.jpg" alt="..." class="img-thumbnail">
+            <div class="bulles_content">
+                <h5>Aventure Particulière</h5>
+                <p>publié le 10/10/20</p>
+                <p><strong>Chapitre 12</strong></p>
+            </div>
+        </div>
+         <div class="col-6">
+            <img src="assets/images/avatar/default.jpg" alt="..." class="img-thumbnail">
+            <div class="bulles_content">
+                <h5>Aventure Particulière</h5>
+                <p>publié le 10/10/20</p>
+                <p><strong>Chapitre 12</strong></p>
+            </div>
+        </div>
+         <div class="col-6">
+            <img src="assets/images/avatar/default.jpg" alt="..." class="img-thumbnail">
+            <div class="bulles_content">
+                <h5>Aventure Particulière</h5>
+                <p>publié le 10/10/20</p>
+                <p><strong>Chapitre 12</strong></p>
+            </div>
+        </div>
+         <div class="col-6">
+            <img src="assets/images/avatar/default.jpg" alt="..." class="img-thumbnail">
+            <div class="bulles_content">
+                <h5>Aventure Particulière</h5>
+                <p>publié le 10/10/20</p>
+                <p><strong>Chapitre 12</strong></p>
+            </div>
+        </div>
+         <div class="col-6">
+            <img src="assets/images/avatar/default.jpg" alt="..." class="img-thumbnail">
+            <div class="bulles_content">
+                <h5>Aventure Particulière</h5>
+                <p>publié le 10/10/20</p>
+                <p><strong>Chapitre 12</strong></p>
+            </div>
+        </div>
+</div>
+
+<h2>Publications</h2>
+<div class="row publications">
+    <strong>Aucune publication</strong>
+</div>
+
+
+<h2>Commentaires</h2>
+<div class="commentaires_bloc">
+			<div class="media">
+				<figure class="media-left">
+					<p class="image is-64x64">
+						<img src="assets/images/avatar/default.jpg">
+					</p>
+				</figure>
+				<div class="media-content">
+                    <p><strong>Jean Neige </strong>- 18/08/20 16:11 </p>
+				    <div class="field">
+				        <p>lndlkndlk</p>
+                    </div>	
+				</div>
+			</div>
+</div>
+
+
