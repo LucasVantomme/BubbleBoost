559a82d6821ac9c7f0201efae046e4e2e8e73302
EternalStay <volff.lorris@gmail.com>
2020-01-24 15:49:49 +0100

    compte.php

--- compte.php
+++ compte.php
@@ -2,41 +2,34 @@
 include 'assets/include/bdd-connect.php';
 include 'assets/include/fonctions.php';
 
-if(!isset($_GET['id']) OR $_GET['id'] == $_SESSION['id'])
+if(!isset($_GET['id']))
 {
-	$page = 'Publications';
-	$titre = 'Mes publications';
-	$connected = true;
+	$page = 'Compte';
+	$titre = 'Mon compte';
 	$user_id = $_SESSION['id'];
 
-	if(isset($_POST['publier-histoire'])) {
-		$result_publier_histoire = form_publier_histoire($_POST, $_FILES, $idcom);
-		if($result_publier_histoire[0]) unset($_POST);
+	if(isset($_POST['updateInformations'])) {
+		$result_updateInformations = form_updateInformations($_POST, $idcom);
+		if($result_updateInformations[0]) unset($_POST);
 	}
+    if(isset($_POST['sendNewMail'])) {
+        $result_sendNewMail = form_sendNewMail($_POST, $idcom);
+        if($result_sendNewMail[0]) unset($_POST);
+    }
+    if(isset($_POST['sendNewPassword'])) {
+        $result_sendNewPassword = form_sendNewPassword($_POST, $idcom);
+        if($result_sendNewPassword[0]) unset($_POST);
+    }
 }
 else {
-	$connected = false;
-	$user_id = $_GET['id'];
+	header('location: profil.php');
 }
 
 // On prend les informations concernant l'utilisateur
-$req = $idcom->prepare('SELECT id, firstname, lastname, username FROM user WHERE id=:id');
+$req = $idcom->prepare('SELECT * FROM user WHERE id=:id');
 $req->bindValue(':id', $user_id, PDO::PARAM_INT);
 $req->execute();
-if($user = $req->fetch()) {
-
-	// Liste de ses histoires
-	$req = $idcom->prepare('SELECT * FROM story WHERE id_user = :id_user ORDER BY title');
-	$req->bindValue(':id_user', $user['id'], PDO::PARAM_INT);
-	$req->execute();
-	$story = $req->fetchAll();
-	
-	$page = 'Publications';
-	$titre = 'Publications - '.$user['firstname'].' '.$user['lastname'];
-
-}
-else
-	header('location: publication.php');
+$user = $req->fetch();
 
 include 'assets/include/header.php';
 ?>
@@ -50,11 +43,17 @@ include 'assets/include/header.php';
     <div class="informations formulaire">
         <h3>Editer mes informations</h3>
 
+        <?php if(isset($result_updateInformations)) { ?>
+            <p>
+                <?php echo $result_updateInformations[1]; ?>
+            </p>
+        <?php } ?>
+
         <form method="POST" action="compte.php">
             <div class="field">
                 <label for="label">Nom</label>
                 <p class="control has-icons-left">
-                    <input class="input" type="text" name="lastname" placeholder="Nom" <?php echo isset($_POST['lastname'])?'value="'.$_POST['lastname'].'"':''; ?> />
+                    <input class="input" type="text" name="lastname" placeholder="Nom" value="<?php echo isset($_POST['lastname'])?$_POST['lastname']:$user['lastname']; ?>" />
                     <span class="icon is-small is-left">
                         <i class="fas fa-user"></i>
                     </span>
@@ -63,7 +62,7 @@ include 'assets/include/header.php';
             <div class="field">
                 <label for="label">Prénom</label>
                 <p class="control has-icons-left">
-                    <input class="input" type="text" name="firstname" placeholder="Prénom" <?php echo isset($_POST['firstname'])?'value="'.$_POST['firstname'].'"':''; ?> />
+                    <input class="input" type="text" name="firstname" placeholder="Prénom" value="<?php echo isset($_POST['firstname'])?$_POST['firstname']:$user['firstname']; ?>" />
                     <span class="icon is-small is-left">
                         <i class="fas fa-user"></i>
                     </span>
@@ -72,7 +71,7 @@ include 'assets/include/header.php';
             <div class="field">
                 <label for="label">Pseudo</label>
                 <p class="control has-icons-left">
-                    <input class="input" type="text" name="username" placeholder="Pseudo" <?php echo isset($_POST['username'])?'value="'.$_POST['username'].'"':''; ?> />
+                    <input class="input" type="text" name="username" placeholder="Pseudo" value="<?php echo isset($_POST['username'])?$_POST['username']:$user['username']; ?>" />
                     <span class="icon is-small is-left">
                         <i class="fas fa-user"></i>
                     </span>
@@ -81,7 +80,7 @@ include 'assets/include/header.php';
             <div class="field">
                 <label for="label">Date de naissance</label>
                 <p class="control has-icons-left">
-                    <input class="input" type="text" name="birthday" placeholder="Date de naissance (JJ/MM/YYYY)" <?php echo isset($_POST['birthday'])?'value="'.$_POST['birthday'].'"':''; ?> />
+                    <input class="input" type="text" name="birthday" placeholder="Date de naissance (JJ/MM/YYYY)" value="<?php echo isset($_POST['birthday'])?$_POST['birthday']:strftime("%d/%m/%G", strtotime($user['birthday_date'])); ?>" />
                     <span class="icon is-small is-left">
                         <i class="fas fa-calendar"></i>
                     </span>
@@ -90,7 +89,7 @@ include 'assets/include/header.php';
             <div class="field">
                 <label for="label">Numéro de téléphone</label>
                 <p class="control has-icons-left">
-                    <input class="input" type="phone" name="phone" placeholder="Téléphone" <?php echo isset($_POST['phone'])?'value="'.$_POST['phone'].'"':''; ?> />
+                    <input class="input" type="text" name="phone" placeholder="Téléphone" value="<?php echo isset($_POST['phone'])?$_POST['phone']:$user['phone']; ?>" />
                     <span class="icon is-small is-left">
                         <i class="fas fa-phone"></i>
                     </span>
@@ -101,97 +100,117 @@ include 'assets/include/header.php';
                 <p class="control has-icons-left">
                     <span class="select">
                         <select class="form-control" name="country" <?php echo isset($_POST['country'])?'value="'.$_POST['country'].'"':''; ?>>
-                            <option value="FRANCE">France</option>
-                            <option value="BELGIQUE">Belgique</option>
-                            <option value="SUISSE">Suisse</option>
-                            <option value="PAYS-BAS">Pays-Bas</option>
-                        </select>
-                    </span>
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-globe"></i>
-                    </span>
-                </p>
+                            <?php
+                            $regions = array("FRANCE" => "France", "BELGIQUE" => "Belgique", "SUISSE" => "Suisse", "PAYS-BAS" => "Pays-Bas");
+                            foreach($regions as $key => $value)
+                            {
+                                ?>
+                                <option value="<?php echo $key; ?>" <?php echo isset($_POST['country'])?(($_POST['country'] == $key)?'selected="selected"':''):(($user['country'] == $key)?'selected="selected"':'');
+                                ?> >
+                                <?php echo $value; ?>
+                            </option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </span>
+                <span class="icon is-small is-left">
+                    <i class="fas fa-globe"></i>
+                </span>
+            </p>
+        </div>
+        <div class="field is-grouped is-grouped-centered">
+            <div class="control">
+                <button class="button is-link is-rounded" name="updateInformations">Modifier mes informations</button>
             </div>
-            <div class="field is-grouped is-grouped-centered">
-                <div class="control">
-                    <button class="button is-link is-rounded" name="updateInformations">Modifier mes informations</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    
-    <hr>
+        </div>
+    </form>
+</div>
 
-    <div class="mail formulaire">
-        <h3>Changer de mail</h3>
+<hr>
 
-        <form method="POST" action="compte.php">
-            <div class="field">
-                <label for="label">Mot de passe</label>
-                <p class="control has-icons-left">
-                    <input class="input" type="password" name="password" placeholder="Mot de passe" />
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-lock"></i>
-                    </span>
-                </p>
-            </div>
-            <div class="field">
-                <label for="lable">Email</label>
-                <p class="control has-icons-left">
-                    <input class="input" type="email" name="email" placeholder="Adresse e-mail" <?php echo isset($_POST['email'])?'value="'.$_POST['email'].'"':''; ?> />
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                </p>
-            </div>
-            <div class="field is-grouped is-grouped-centered">
-                <div class="control">
-                    <button class="button is-link is-rounded" name="sendNewMail">Envoyer</button>
-                </div>
-            </div>
-        </form>
-    </div>
+<div class="mail formulaire">
+    <h3>Changer de mail</h3>
 
-    <hr>
+    <?php if(isset($result_sendNewMail)) { ?>
+        <p>
+            <?php echo $result_sendNewMail[1]; ?>
+        </p>
+    <?php } ?>
 
-    <div class="password formulaire">
-        <h3>Changer de mot de passe</h3>
+    <form method="POST" action="compte.php">
+        <div class="field">
+            <label for="label">Mot de passe</label>
+            <p class="control has-icons-left">
+                <input class="input" type="password" name="password" placeholder="Mot de passe" />
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+        </div>
+        <div class="field">
+            <label for="lable">Email</label>
+            <p class="control has-icons-left">
+                <input class="input" type="email" name="mail" placeholder="Adresse e-mail" value="<?php echo isset($_POST['mail'])?$_POST['mail']:$user['mail']; ?>" />
+                <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                </span>
+            </p>
+        </div>
+        <div class="field is-grouped is-grouped-centered">
+            <div class="control">
+                <button class="button is-link is-rounded" name="sendNewMail">Envoyer</button>
+            </div>
+        </div>
+    </form>
+</div>
 
-        <form method="POST" action="compte.php">
-            <div class="field">
-                <label for="label">Ancien mot de passe</label>
-                <p class="control has-icons-left">
-                    <input class="input" type="password" name="oldPassword" placeholder="Ancien mot de passe" />
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-lock"></i>
-                    </span>
-                </p>
+<hr>
+
+<div class="password formulaire">
+    <h3>Changer de mot de passe</h3>
+
+    <?php if(isset($result_sendNewPassword)) { ?>
+        <p>
+            <?php echo $result_sendNewPassword[1]; ?>
+        </p>
+    <?php } ?>
+
+    <form method="POST" action="compte.php">
+        <div class="field">
+            <label for="label">Ancien mot de passe</label>
+            <p class="control has-icons-left">
+                <input class="input" type="password" name="oldPassword" placeholder="Ancien mot de passe" />
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+        </div>
+        <div class="field">
+            <label for="label">Nouveau mot de passe</label>
+            <p class="control has-icons-left">
+                <input class="input" type="password" name="newPassword" placeholder="Nouveau mot de passe" />
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+        </div>
+        <div class="field">
+            <label for="label">Confirmation du nouveau mot de passe</label>
+            <p class="control has-icons-left">
+                <input class="input" type="password" name="confirmNewPassword" placeholder="Confirmation du nouveau mot de passe" />
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+        </div>
+        <div class="field is-grouped is-grouped-centered">
+            <div class="control">
+                <button class="button is-link is-rounded" name="sendNewPassword">Confirmer</button>
             </div>
-            <div class="field">
-                <label for="label">Nouveau mot de passe</label>
-                <p class="control has-icons-left">
-                    <input class="input" type="password" name="newPassword" placeholder="Nouveau mot de passe" />
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-lock"></i>
-                    </span>
-                </p>
-            </div>
-            <div class="field">
-                <label for="label">Confirmation du nouveau mot de passe</label>
-                <p class="control has-icons-left">
-                    <input class="input" type="password" name="confirmNewPassword" placeholder="Confirmation du nouveau mot de passe" />
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-lock"></i>
-                    </span>
-                </p>
-            </div>
-            <div class="field is-grouped is-grouped-centered">
-                <div class="control">
-                    <button class="button is-link is-rounded" name="confirmNewPassword">Confirmer</button>
-                </div>
-            </div>
-        </form>
-    </div>
+        </div>
+    </form>
+</div>
 </div>
 
 <?php include 'assets/include/footer.php'; ?>
