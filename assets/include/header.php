4e257902c6d5c5e0faa03bd02abc07837326b5d1
EternalStay <volff.lorris@gmail.com>
2020-01-28 14:09:53 +0100

    fix

--- assets/include/header.php
+++ assets/include/header.php
@@ -11,7 +11,7 @@ include_once 'fonctions.php';
 	<meta charset="UTF-8">
 	<title><?php echo $titre; ?></title>
 	<link rel="stylesheet" href="/BubbleBoost/assets/css/styles.css">
-    <!--<link rel="stylesheet" href="/BubbleBoost/assets/css/profil.css">-->
+	<?php if($page == "Profil") { ?> <link rel="stylesheet" href="/BubbleBoost/assets/css/profil.css"> <?php } ?>
 	<?php if($page == "Publications") { ?> <link rel="stylesheet" href="/BubbleBoost/assets/css/mes-publications.css"> <?php } ?>
 	<?php if($page == "Accueil") { ?> <link rel="stylesheet" href="/BubbleBoost/assets/css/accueil.css"> <?php } ?>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
